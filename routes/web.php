62386e0ab8ca2dec666439769698f71a9d2ed919
moosbeere_O <moosbeere_O@mail.ru>
2024-11-14 19:05:58 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Authenticate
+Route::get('/auth/signup', [AuthController::class, 'signup']);
+Route::post('/auth/registr', [AuthController::class, 'registr']);
+
+
+
+
+Route::get('/', [MainController::class, 'index']);
+Route::get('galery/{img}/{name}', function($img, $name){
+    return view('main.galery', ['img'=>$img, 'name'=>$name]);
+});
+
+Route::get('/about', function(){
+    return view('main.about');
+});
+
+Route::get('/contacts', function(){
+    $data = [
+        "city"=>"Moscow",
+        "street"=>"Semenovskaya",
+        "house"=>38
+    ];
+
+    return view('main.contact', ['data'=>$data]);
+});
+
+// Route::get('/', function () {
+//     return view('layout');
+// });
